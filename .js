f1252bf3be73e4e910680afa0f1507df53fe34a5
ImACitrus <109486300+ImACitrus@users.noreply.github.com>
2022-12-11 23:09:08 +0100

    Update and rename README.md to .js

---
+++ .js
@@ -0,0 +1,15 @@
+// About
+/*
+
+  I do not own this hub,
+  I republish this library due to use,
+  In order for me to use this menu is adding,
+  A safety feature to the library itself,
+  
+  Publisher:
+  @Stebulous
+  
+  Source:
+  https://raw.githubusercontent.com/Stebulous/solaris-ui-lib/main/source.lua
+  
+*/
